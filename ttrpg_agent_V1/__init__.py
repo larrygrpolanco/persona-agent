7f00dbf6af2b3a286e0f076076dec53ba0dd2c6f
Larry Grullon-Polanco <larrygrpolanco@gmail.com>
2025-04-28 15:00:52 +0800

    Code Breakdown

--- ttrpg_agent_V1/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-TTRPG Generative Agent package.
-
-This package adapts the Generative Agents architecture for use in tabletop
-role-playing games (TTRPGs), specifically the game ECH0.
-"""
-
-__version__ = "0.1.0"
